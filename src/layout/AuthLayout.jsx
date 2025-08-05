5d29dc6c75729f9f9bb7c1e7e3a5a185c16a5b48
yogeshsingh-1 <yogesh.singh@mygstcafe.in>
2025-08-05 20:57:12 +0530

    p

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
+
+{/*
+     <AuthLayout>
+  ├── Logo (top center)
+  ├── Title: "Welcome Back!"
+  ├── <Outlet /> → Login / Register form
+  ├── Link: “Don’t have account? Register”
+  └── Footer: “© 2025 MyApp”
+</AuthLayout> 
+*/}
